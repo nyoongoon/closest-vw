5cb686165de0e21ed1b2e490ec53f880dbbf53d3
nyoongoon <nyoongoon@naver.com>
2024-10-22 07:57:11 +0900

    first commit

---
+++ src/main/java/com/example/closestv2/ClosestV2Application.java
@@ -0,0 +1,13 @@
+package com.example.closestv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosestV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClosestV2Application.class, args);
+    }
+
+}
