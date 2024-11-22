74a0f9350888fab3bbc200f6640f07913c8a4e78
nyoongoon <nyoongoon@naver.com>
2024-11-22 22:04:00 +0900

    비동기 방식으로 update 수정

--- src/main/java/com/example/closestv2/ClosestV2Application.java
+++ src/main/java/com/example/closestv2/ClosestV2Application.java
@@ -2,8 +2,10 @@ package com.example.closestv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
 @EnableScheduling
 @SpringBootApplication
 public class ClosestV2Application {
--- src/main/java/com/example/closestv2/api/service/BlogSchedulerService.java
+++ src/main/java/com/example/closestv2/api/service/BlogSchedulerService.java
@@ -4,6 +4,10 @@ import com.example.closestv2.domain.blog.BlogFactory;
 import com.example.closestv2.domain.blog.BlogRepository;
 import com.example.closestv2.domain.blog.BlogRoot;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,30 +16,47 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class BlogSchedulerService {
+public class BlogSchedulerService { // 이런 서비스 레이어의 테스트 -> 통합테스트 -> mock을 잘 안쓰나..?
     private final BlogFactory blogFactory;
     private final BlogRepository blogRepository;
 
+    private static final int PAGE_SIZE = 100;
+
     @Scheduled(fixedDelay = 5000)
-    @Transactional
-    public void pollingUpdatedBlogs() throws MalformedURLException, URISyntaxException {
-        //TODO 페이징이 필요할까?
-        List<BlogRoot> blogRoots = blogRepository.findAll();
-        for (BlogRoot blogRoot : blogRoots) {
-//            pollingIfUpdated(blogRoot); // 개별 트랜잭션으로 비동기적으로 처리?
+    public void pollingUpdatedBlogs() {
+        int page = 0;
+        boolean hasMore = true;
+
+        while(hasMore){
+            Page<BlogRoot> blogPage = blogRepository.findAll(PageRequest.of(page, PAGE_SIZE));
+            List<BlogRoot> blogRoots = blogPage.getContent();
+            hasMore = blogPage.hasNext();
+
+            for (BlogRoot blogRoot : blogRoots) {
+                pollingIfUpdated(blogRoot);
+            }
+            page++;
         }
     }
 
+    @Async
+    @Transactional
+    public void pollingIfUpdated(BlogRoot blogRoot) {
+        try {
+            BlogRoot recentBlogRoot = blogFactory.createRecentBlogRoot(blogRoot.getBlogInfo().getBlogUrl());
+            boolean isBlogUpdated = blogRoot.isBlogUpdated(recentBlogRoot);
 
-    private void pollingIfUpdated(BlogRoot blogRoot) throws MalformedURLException, URISyntaxException {
-        //blogFactory는 외부 시스템 의존 중.. -> 테스트는 모두 mock 처리?
-        BlogRoot recentBlogRoot = blogFactory.createRecentBlogRoot(blogRoot.getBlogInfo().getBlogUrl());
-        boolean isBlogUpdated = blogRoot.isBlogUpdated(recentBlogRoot);
-        if(!isBlogUpdated){
-            return;
+            if (isBlogUpdated) {
+                blogRoot.updateBlogRoot(recentBlogRoot);
+                blogRepository.save(blogRoot); // 업데이트 내용을 저장
+            }
+        } catch (MalformedURLException | URISyntaxException e) {
+            // 에러 로그 처리, 로깅 사용 권장
+            log.error("블로그 업데이트 중 에러 발생 - url :{}", blogRoot.getBlogInfo().getBlogUrl().toString());
+            log.error("에러 메시지 : {}", e.getMessage());
         }
-        blogRoot.updateBlogRoot(recentBlogRoot);
     }
 }
