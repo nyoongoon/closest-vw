598c5d6b2fd137221a970a9a45289398843be69d
nyoongoon <nyoongoon@naver.com>
2024-12-12 08:12:04 +0900

    SubscriptionQueryDslQueryRepository 테스트 작성

--- src/test/java/com/example/closestv2/infrastructure/domain/subscription/SubscriptionQueryDslQueryRepositoryTest.java
+++ src/test/java/com/example/closestv2/infrastructure/domain/subscription/SubscriptionQueryDslQueryRepositoryTest.java
@@ -31,15 +31,15 @@ class SubscriptionQueryDslQueryRepositoryTest extends RepositoryTestSupport {
     @DisplayName("memberId로 visitCount 내림차순 정렬하여 조회한다.")
     void findByMemberIdVisitCountDesc() throws MalformedURLException {
         //given
-        SubscriptionRoot subscriptionRoot1 = SubscriptionRoot.create(1L, URI.create(ANY_BLOG_LINK + 1).toURL(), ANY_BLOG_TITLE, ANY_PUBLISHED_DATE_TIME);
+        SubscriptionRoot subscriptionRoot1 = SubscriptionRoot.create(1L, URI.create(ANY_BLOG_LINK + 1).toURL(), ANY_BLOG_TITLE, ANY_PUBLISHED_DATE_TIME.minusSeconds(1));
         subscriptionRoot1.increaseVisitCount();
         subscriptionRepository.save(subscriptionRoot1);
-        SubscriptionRoot subscriptionRoot2 = SubscriptionRoot.create(1L, URI.create(ANY_BLOG_LINK + 2).toURL(), ANY_BLOG_TITLE, ANY_PUBLISHED_DATE_TIME);
+        SubscriptionRoot subscriptionRoot2 = SubscriptionRoot.create(1L, URI.create(ANY_BLOG_LINK + 2).toURL(), ANY_BLOG_TITLE, ANY_PUBLISHED_DATE_TIME.minusSeconds(2));
         for (int i = 0; i < 3; i++) {
             subscriptionRoot2.increaseVisitCount();
         }
         subscriptionRepository.save(subscriptionRoot2);
-        SubscriptionRoot subscriptionRoot3 = SubscriptionRoot.create(1L, URI.create(ANY_BLOG_LINK + 3).toURL(), ANY_BLOG_TITLE, ANY_PUBLISHED_DATE_TIME);
+        SubscriptionRoot subscriptionRoot3 = SubscriptionRoot.create(1L, URI.create(ANY_BLOG_LINK + 3).toURL(), ANY_BLOG_TITLE, ANY_PUBLISHED_DATE_TIME.minusSeconds(3));
         for (int i = 0; i < 2; i++) {
             subscriptionRoot3.increaseVisitCount();
         }
@@ -48,23 +48,40 @@ class SubscriptionQueryDslQueryRepositoryTest extends RepositoryTestSupport {
         //when
         List<SubscriptionRoot> subscriptionRoots = sut.findByMemberIdVisitCountDesc(1L, 0, 10);
         //then
-        assertThat(subscriptionRoots).hasSize(2);
+        assertThat(subscriptionRoots).hasSize(3)
+                .isSortedAccordingTo((x, y) -> Math.toIntExact(y.getSubscriptionInfo().getSubscriptionVisitCount() - x.getSubscriptionInfo().getSubscriptionVisitCount()));
     }
 
     @Test
     @DisplayName("memberId로 visitCount 내림차순 정렬 조회 시 페이징한다.")
     void findByMemberIdVisitCountDescPaging() throws MalformedURLException {
         //given
+        for (int i = 0; i < 17; i++) {
+            SubscriptionRoot subscriptionRoot1 = SubscriptionRoot.create(1L, URI.create(ANY_BLOG_LINK + i).toURL(), ANY_BLOG_TITLE, ANY_PUBLISHED_DATE_TIME);
+            subscriptionRoot1.increaseVisitCount();
+            subscriptionRepository.save(subscriptionRoot1);
+        }
         //when
+        List<SubscriptionRoot> subscriptionRoots = sut.findByMemberIdVisitCountDesc(1L, 1, 10);
         //then
+        assertThat(subscriptionRoots).hasSize(7);
     }
 
     @Test
     @DisplayName("memberId로 publishedDateTime 내림차순 정렬하여 조회힌다.")
-    void findByMemberIdPublishedDateTimeDesc(){
+    void findByMemberIdPublishedDateTimeDesc() throws MalformedURLException {
         //given
+        SubscriptionRoot subscriptionRoot1 = SubscriptionRoot.create(1L, URI.create(ANY_BLOG_LINK + 1).toURL(), ANY_BLOG_TITLE, ANY_PUBLISHED_DATE_TIME.minusSeconds(1));
+        subscriptionRepository.save(subscriptionRoot1);
+        SubscriptionRoot subscriptionRoot2 = SubscriptionRoot.create(1L, URI.create(ANY_BLOG_LINK + 2).toURL(), ANY_BLOG_TITLE, ANY_PUBLISHED_DATE_TIME.minusSeconds(2));
+        subscriptionRepository.save(subscriptionRoot2);
+        SubscriptionRoot subscriptionRoot3 = SubscriptionRoot.create(1L, URI.create(ANY_BLOG_LINK + 3).toURL(), ANY_BLOG_TITLE, ANY_PUBLISHED_DATE_TIME.minusSeconds(3));
+        subscriptionRepository.save(subscriptionRoot3);
         //when
+        List<SubscriptionRoot> subscriptionRoots = sut.findByMemberIdPublishedDateTimeDesc(1L, 0, 10);
         //then
+        assertThat(subscriptionRoots).hasSize(3)
+                .isSortedAccordingTo((x, y) -> y.getSubscriptionBlog().getPublishedDateTime().compareTo(x.getSubscriptionBlog().getPublishedDateTime()));
     }
 
     @Test
